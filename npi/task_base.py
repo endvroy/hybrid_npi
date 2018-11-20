b1815036f1b30ac9cd0b4a24b40e497203bd0ea1
Ruoyi Wang <endvroy@gmail.com>
2018-11-19 19:40:07 -0800

    updated doc in TaskBase

--- npi/task_base.py
+++ npi/task_base.py
@@ -8,23 +8,32 @@ class TaskBase(nn.Module, abc.ABC):
     abstract base class for tasks
     for each task, inherit and overwrite the methods
     note the type of the return value depends on the state dimension
-
     """
 
-    def __init__(self, env, state_dim, batch_size=1):
+    def __init__(self, env, state_dim):
+        """
+        :param env: a list of initial env
+        :param state_dim: dimension of state tensor
+        """
         super(TaskBase, self).__init__()
         self.init_env = copy.copy(env)
-        self.env = [copy.copy(env) for i in range(batch_size)]
+        self.env = env
         self.state_dim = state_dim
-        self.batch_size = batch_size
+        self.batch_size = len(env)
 
     def reset(self):
-        self.env = [copy.copy(self.init_env) for i in range(self.batch_size)]
+        self.env = copy.copy(self.init_env)
 
     @abc.abstractmethod
     def f_enc(self, args):
-        """encoder fn:: (args: Tensor[args_dim]) -> Tensor[state_dim]"""
+        """
+        encoder fn:: (args: Tensor[args_dim]) -> Tensor[state_dim]
+        all args are in batches
+        """
 
     @abc.abstractmethod
     def f_env(self, prog_id, args):
-        """env fn:: (args: Tensor[args_dim], prog_id: int)"""
+        """
+        env fn:: (args: Tensor[args_dim], prog_id: int)
+        all args are in batches
+        """
