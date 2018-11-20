341133ffca26911a87e089a7dad75d0be777d0a2
VertexC <bob2420083992@gmail.com>
2018-11-19 20:02:57 -0800

    revise task

--- tasks/addition/task.py
+++ tasks/addition/task.py
@@ -12,17 +12,54 @@ WRITE_OUT, WRITE_CARRY = 0, 1
 IN1_PTR, IN2_PTR, CARRY_PTR, OUT_PTR = range(4)
 LEFT, RIGHT = 0, 1
 
-def build(in1s, in2s, hidden_dim, state_dim, environment_row, environment_col, environment_depth, argument_num, argument_depth, default_argument_num, program_embedding_size, program_size, batch_size=1):
-    scratchPads = []
+def build(in1s, 
+          in2s, 
+          hidden_dim, 
+          state_dim, 
+          environment_row, 
+          environment_col, 
+          environment_depth, 
+          argument_num, 
+          argument_depth, 
+          default_argument_num, 
+          program_embedding_size, 
+          program_size, 
+          batch_size=1):
+    scratch_pads = []
     for i in range(batch_size):
-        scratchPads.append(ScratchPad(in1s[i], in2s[i], environment_row, environment_col))
-    envs = [scratchPads[i].get_env(environment_row, environment_col, environment_depth) for i in range(batch_size)]
-    task = AdditionCore(scratchPads, envs, hidden_dim, state_dim, environment_row, environment_col, environment_depth, argument_num, argument_depth, default_argument_num, program_embedding_size, program_size, batch_size=1)
+        scratch_pads.append(ScratchPad(in1s[i], in2s[i], environment_row, environment_col))
+    envs = [scratch_pads[i].get_env(environment_row, environment_col, environment_depth) for i in range(batch_size)]
+    task = AdditionTask(scratch_pads=scratch_pads, 
+                        envs=envs, 
+                        hidden_dim=hidden_dim, 
+                        state_dim=state_dim, 
+                        environment_row=environment_row, 
+                        environment_col=environment_col, 
+                        environment_depth=environment_depth, 
+                        argument_num=argument_num, 
+                        argument_depth=argument_depth, 
+                        default_argument_num=default_argument_num, 
+                        program_embedding_size=program_embedding_size, 
+                        program_size=program_size, 
+                        batch_size=batch_size)
     return task
 
-class AdditionCore(TaskBase):
-    def __init__(self, scratchPads, envs, hidden_dim, state_dim, environment_row, environment_col, environment_depth, argument_num, argument_depth, default_argument_num, program_embedding_size, program_size, batch_size=1):
-        super(AdditionCore, self).__init__(envs, state_dim, batch_size)
+class AdditionTask(TaskBase):
+    def __init__(self, 
+                scratch_pads, 
+                envs, 
+                hidden_dim, 
+                state_dim, 
+                environment_row, 
+                environment_col, 
+                environment_depth, 
+                argument_num, 
+                argument_depth, 
+                default_argument_num, 
+                program_embedding_size, 
+                program_size, 
+                batch_size=1):
+        super(AdditionTask, self).__init__(envs, state_dim, batch_size)
         # config params
         self.environment_row = environment_row
         self.environment_col = environment_col
@@ -40,7 +77,7 @@ class AdditionCore(TaskBase):
         self.in_dim = self.env_dim + self.arg_dim
         self.prog_embedding_dim = program_embedding_size
         self.prog_dim = program_size
-        self.scratchPads = copy.copy(scratchPads)
+        self.scratch_pads = copy.copy(scratch_pads)
         # for f_enc
         self.fenc1 = nn.Linear(self.in_dim, self.hidden_dim)
         self.fenc2 = nn.Linear(self.hidden_dim, self.hidden_dim)
@@ -50,7 +87,7 @@ class AdditionCore(TaskBase):
       
     def init_scratchPad(self, in1s, in2s):
         for i in range(self.batch_size):
-            self.scratchPads[i].init_scratchpad(in1s[i], in2s[i])
+            self.scratch_pads[i].init_scratchpad(in1s[i], in2s[i])
     
     def get_args(self, args, arg_in=True):
         if arg_in:
@@ -70,7 +107,7 @@ class AdditionCore(TaskBase):
         return [arg_vec.flatten() if arg_in else arg_vec]
 
     def f_enc(self, args):
-        env = [self.scratchPads[i].get_env() for i in range(self.batch_size)]
+        env = [self.scratch_pads[i].get_env() for i in range(self.batch_size)]
         env = torch.stack(env, -1)
         merge = torch.cat([env, args], -1)
         elu = F.elu(self.fenc1(merge))
@@ -78,7 +115,7 @@ class AdditionCore(TaskBase):
         out = self.fenc3(elu)
         return out 
 
-    def get_program_embedding(self, prog_id, args):
+    def get_program_embedding(self, prog_id):
         embedding = self.fenv1(prog_id)
         return embedding
     
@@ -86,5 +123,5 @@ class AdditionCore(TaskBase):
         for i in range(self.batch_size):
             prog_id = prog_ids[i]
             if prog_id == MOVE_PID or prog_id == WRITE_PID:
-                self.scratchPads[i].execute(prog_id, args)
-        return [self.scratchPads[i].get_env(self.environment_row, self.environment_col, self.environment_depth)]
+                self.scratch_pads[i].execute(prog_id, args)
+        return [self.scratch_pads[i].get_env(self.environment_row, self.environment_col, self.environment_depth) for i in range(self.batch_size)]
