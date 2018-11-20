98e6bcd3a13eef086fd26720a160fce381e7c1d6
jcjerbear <cyc42@sfu.ca>
2018-11-19 20:36:21 -0800

    refactored dir paths according to structure change

--- tasks/addition/env/data_generation.py
+++ tasks/addition/env/data_generation.py
@@ -8,9 +8,7 @@ called.
 """
 import json
 import numpy as np
-from env.trace import Trace
-
-
+from tasks.addition.env.trace import Trace
 
 
 def generate_addition(prefix, num_examples, debug=False, maximum=10000000000, debug_every=1000):
@@ -22,7 +20,7 @@ def generate_addition(prefix, num_examples, debug=False, maximum=10000000000, de
     :param num_examples: Number of examples to generate.
 
     """
-    int_data = []
+    in_data = []
     trace_data = []
     for i in range(num_examples):
         in1 = np.random.randint(maximum - 1)
@@ -31,18 +29,18 @@ def generate_addition(prefix, num_examples, debug=False, maximum=10000000000, de
             trace = Trace(in1, in2, True).trace
         else:
             trace = Trace(in1, in2).trace
-        int_data.append((in1, in2))
+        in_data.append((in1, in2))
         trace_data.append(trace)
 
     with open('../data/{}.json'.format(prefix+"_int"), 'w') as f:
-        json.dump(int_data, f)
+        json.dump(in_data, f)
     with open('../data/{}.json'.format(prefix+"_trace"), 'w') as f:
         json.dump(trace_data, f)
 
 
 if __name__ == '__main__':
     num_training = 1
-    TRAINING_INT_DATA_PATH = "../data/train_int.json"
+    TRAINING_INT_DATA_PATH = "../data/train_in.json"
     TRAINING_TRACE_DATA_PATH = "../data/train_trace.json"
 
     # int numbers
