1eb8a27e9cf12e4293cec7c514bbab8119d1b792
zhuwangz <zhuwangz@sfu.ca>
2018-11-23 02:36:35 -0800

    :sparkles: train runs successfully

--- src/train/adapter.py
+++ src/train/adapter.py
@@ -1,34 +1,41 @@
 import json
 
 
-# element of input_trace is dict={'ret':B*T*1,'prog_id:B*T*1','args:B*T*x'}
+# element of input_trace is dict={'ret':T*B*1,'prog_id:T*B*1','args:T*B*x'}
 def trace_json_to_input(batchsize, args_dim, padding, in_file, out_file):
     with open(in_file, 'r') as fin:
         # element of traces is list=[[["prog_name",prog_id],args,ret],[...],[...]]
         traces = json.load(fin)
 
     traces_input = []
-    padding_token = [[0], [0] * args_dim, 1]
-    for i in range(len(traces)):
+    padding_token = [0, [0] * args_dim, 1]
+    for i in range(0, len(traces), batchsize):
         if i + batchsize > len(traces):
             break
-        trace_input = {'ret': [], 'prog_id': [], 'args': []}
         max_length = 0
         for j in range(batchsize):
-            trace_input['ret'].append([])
-            trace_input['prog_id'].append([])
-            trace_input['args'].append([])
-            for step in traces[i + j]:
-                trace_input['ret'][j].append([1 if step[2] == True else 0])
-                trace_input['prog_id'][j].append([step[0][1]])
-                trace_input['args'][j].append(step[1] + [0] * (3 - len(step[1])))
-            if len(trace_input['ret'][j]) > max_length:
-                max_length = len(trace_input['ret'][j])
-        if padding:
+            if max_length < len(traces[i + j]):
+                max_length = len(traces[i + j])
+        trace_input = {'ret': [], 'prog_id': [], 'args': [], 'len': max_length}
+        trace_input['ret'] = []
+        trace_input['prog_id'] = []
+        trace_input['args'] = []
+        for l in range(max_length):
+            ret_input = []
+            prog_input = []
+            args_input = []
             for j in range(batchsize):
-                trace_input['ret'][j] += [padding_token[2]] * (max_length - len(trace_input['ret'][j]))
-                trace_input['prog_id'][j] += [padding_token[0]] * (max_length - len(trace_input['prog_id'][j]))
-                trace_input['args'][j] += [padding_token[1]] * (max_length - len(trace_input['args'][j]))
+                if len(traces[i + j]) <= l:
+                    ret_input.append(padding_token[2])
+                    prog_input.append(padding_token[0])
+                    args_input.append(padding_token[1])
+                else:
+                    ret_input.append(1 if traces[i + j][l][2] == True else 0)
+                    prog_input.append(traces[i + j][l][0][1])
+                    args_input.append(traces[i + j][l][1] + [0] * (args_dim - len(traces[i + j][l][1])))
+            trace_input['ret'].append(ret_input)
+            trace_input['prog_id'].append(prog_input)
+            trace_input['args'].append(args_input)
         traces_input.append(trace_input)
 
     with open(out_file, 'w') as fout:
@@ -36,6 +43,6 @@ def trace_json_to_input(batchsize, args_dim, padding, in_file, out_file):
 
 
 if __name__ == "__main__":
-    trace_json_to_input(batchsize=1, args_dim=3, padding=True,
+    trace_json_to_input(batchsize=2, args_dim=3, padding=True,
                         in_file="./src/tasks/addition/data/train_trace.json",
                         out_file="./src/tasks/addition/data/train_trace_input.json")
--- src/train/train.py
+++ src/train/train.py
@@ -1,9 +1,10 @@
-from models.npi import npi_factory
-import sys, time, math, json, random
+from src.models.npi import npi_factory
+import sys, time, math, json, random, os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from tasks.task_base import TaskBase
+from torch.autograd import Variable
+from src.tasks.task_base import TaskBase
 
 PRETRAIN_WRIGHT_DECAY = 0.00001
 PRETRAIN_LR = 0.0001
@@ -19,10 +20,11 @@ class Agent(object):
         self.pretrain_optimizer = optim.Adam(list_of_params, lr=PRETRAIN_LR,
                                              weight_decay=PRETRAIN_WRIGHT_DECAY)
         self.criterion_nll = nn.NLLLoss()
-        self.criterion_cross = nn.CrossEntropyLoss()
+        self.criterion_binary = nn.BCELoss()
+        self.criterion_mse = nn.MSELoss()
 
 
-def train(npi, data, trace, epochs=10):
+def train(npi, data, traces, batchsize, epochs=10):
     agent = Agent(npi)
     agent.npi.train()
     start_time = time.time()
@@ -33,58 +35,56 @@ def train(npi, data, trace, epochs=10):
         # parameters
         total_loss = 0
         total_trace = 0
-        arg_error = torch.zeros(agent.npi.core.args_dim)
-        prog_error = torch.zeros(agent.npi.n_progs)
-        ret_error = torch.zeros(1)
+        arg_loss = 0
+        prog_loss = 0
+        ret_loss = 0
 
-        for d in data:
-            agent.npi.task = d
+        for i in range(len(data)):
+            agent.npi.task = data[i]
 
             # Setup Environment
             agent.npi.core.reset()
 
             # trace one by one
-            for t in range(len(trace) - 1):
-                prog_id = trace[t]["prog_id"]
-                prog_id_log_probs = torch.zeros(agent.npi.n_progs)
-                prog_id_log_probs[int(prog_id[0])] = 1.0
-                args = trace[t]["args"]
+            trace = traces[i]
+            for t in range(trace['len'] - 1):
+                prog_id = torch.tensor(trace["prog_id"][t])
+                args = torch.tensor(trace["args"][t])
 
                 # forward
-                new_ret, new_prog_id_log_probs, new_args = agent.npi.forward(prog_id, args)
-                new_prog_id = torch.argmax(new_prog_id_log_probs, dim=1)
+                agent.pretrain_optimizer.zero_grad()
+                new_ret, new_prog_id_log_probs, new_args = agent.npi(prog_id, args)
 
                 # loss
                 # new_para = torch.cat([new_ret, new_prog_id_log_probs, new_args], -1)
-                truth_ret = trace[t + 1]["ret"]
-                truth_prog_id = trace[t + 1]["prog_id"]
-                truth_prog_id_log_probs = torch.zeros(agent.npi.n_progs)
-                truth_prog_id_log_probs[int(truth_prog_id[0])] = 1.0
-                truth_args = trace[t + 1]["args"]
+                truth_ret = torch.tensor(trace["ret"][t + 1], dtype = torch.float32)
+                truth_prog_id = torch.tensor(trace["prog_id"][t + 1])
+                truth_prog_id_log_probs = torch.zeros(batchsize, agent.npi.n_progs)
+                for i in range(batchsize):
+                    truth_prog_id_log_probs[i][int(truth_prog_id[i])] = 1.0
+                truth_args = torch.tensor(trace["args"][t + 1], dtype=torch.float32)
                 # truth_para = torch.cat([truth_ret, truth_prog_id_log_probs, truth_args], -1)
 
-                loss_batch = torch.cat([agent.criterion_cross(new_ret, truth_ret),
-                                        agent.criterion_nll(new_prog_id, truth_prog_id),
-                                        agent.criterion_cross(new_args, truth_args)], -1)
-
-                arg_error += (truth_args - new_args) ** 2
-                ret_error += (truth_ret - new_ret) ** 2
-                prog_error += (truth_prog_id_log_probs - new_prog_id_log_probs) ** 2
-                total_loss += (loss_batch ** 2).sum().to(dtype=torch.float32)
+                arg_loss += agent.criterion_mse(new_args, truth_args)
+                ret_loss += agent.criterion_mse(new_ret, truth_ret)
+                prog_loss += agent.criterion_nll(new_prog_id_log_probs, truth_prog_id)
+                loss_batch = arg_loss + ret_loss + prog_loss
 
                 # backpropagation
-                agent.pretrain_optimizer.zero_grad()
-                loss_batch.backward()
+                # if t != trace['len'] - 2:
+                #     loss_batch.backward(retain_graph=True)
+                # else:
+                loss_batch.backward(retain_graph=True)
                 agent.pretrain_optimizer.step()
+              
+            total_trace += trace['len'] - 1
 
-            total_trace += (len(trace) - 1)
-
-        arg_error /= total_trace
-        ret_error /= total_trace
-        prog_error /= total_trace
+        arg_loss /= total_trace
+        ret_loss /= total_trace
+        prog_loss /= total_trace
         end_time = time.time()
         print("Epoch {}: Ret error {}, Arg_error {}, Prog_error {}, Time {}"
-              .format(ep, ret_error, arg_error, prog_error, end_time - start_time))
+              .format(ep, ret_loss, arg_loss, prog_loss, end_time - start_time))
 
         # save model
         if total_loss < best_loss:
@@ -96,6 +96,8 @@ def train(npi, data, trace, epochs=10):
                 'npi_core_state_dict': agent.npi.core.state_dict(),
                 'optimizer': agent.pretrain_optimizer.state_dict()
             }
+            if not os.path.isdir("./model"):
+                os.mkdir("./model")
             torch.save(save_state, 'model/npi_model_latest.net')
 
 
@@ -107,7 +109,7 @@ if __name__ == "__main__":
 
     state_dim = 2
     args_dim = 3
-    data_num = 100
+    data_num = 2
 
 
     class DummyTask(TaskBase):
@@ -124,14 +126,15 @@ if __name__ == "__main__":
     data = []
     trace = []  # element of trace is dict={'ret':xx,'prog_id:xx','args:xx'}
     for i in range(data_num):
-        dummy_task = DummyTask([random.randint(1, 1000)], state_dim)
+        dummy_task = DummyTask([random.randint(1, 1000), random.randint(1, 1000)], state_dim)
         data.append(dummy_task)
     with open("./src/tasks/addition/data/train_trace_input.json", 'r') as fin:
         trace = json.load(fin)
-
+        trace.append(trace[0])
+      
     npi = npi_factory(task=dummy_task,
                       state_dim=state_dim,
-                      n_progs=3,
+                      n_progs=5,
                       prog_dim=5,
                       hidden_dim=3,
                       n_lstm_layers=2,
@@ -141,4 +144,7 @@ if __name__ == "__main__":
                       n_act=2)
     print('Initializing NPI Model!')
 
-    train(npi, data, trace)
+    assert len(data) <= len(trace)
+    print("Data:", len(data))
+    print("Traces:", len(trace))
+    train(npi, data, trace, batchsize=2)
